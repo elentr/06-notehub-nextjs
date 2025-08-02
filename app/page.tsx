0176d9393d7d88159e8f62b478484884a7044fde
elentr <tr.13.elena@gmail.com>
2025-08-02 22:53:05 +0300

    starting work on hw

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+// import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>
+          <h1>Welcome to NoteHub</h1>
+          <p>
+            NoteHub is a simple and efficient application designed for managing
+            personal notes. It helps keep your thoughts organized and accessible
+            in one place, whether you are at home or on the go.
+          </p>
+          <p>
+            The app provides a clean interface for writing, editing, and
+            browsing notes. With support for keyword search and structured
+            organization, NoteHub offers a streamlined experience for anyone who
+            values clarity and productivity.
+          </p>
+        </div>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
---
+++ lib/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export type Note = {
+  id: string;
+  title: string;
+  content: string;
+  categoryId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type NoteListResponse = {
+  notes: Note[];
+  total: number;
+};
+
+axios.defaults.baseURL = 'https://next-docs-api.onrender.com';
+
+export const getNotes = async () => {
+  const res = await axios.get<NoteListResponse>('/notes');
+  return res.data;
+};
+
+// const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+// export const getNotes = async () => {
+//   await delay(2000);
+//   const res = await axios.get<NoteListResponse>('/notes');
+//   return res.data;
+// };
+
+export const getSingleNote = async (id: string) => {
+  const res = await axios.get<Note>(`/notes/${id}`);
+  return res.data;
+};
